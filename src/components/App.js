5558420efd2fe93eac2121e1e8977094cdbc9ef0
kiran <koffeewithkiran77@gmail.com>
2019-07-30 12:13:03 +0530

    stage 1

---
+++ src/components/App.js
@@ -0,0 +1,27 @@
+import React from "react";
+import { BrowserRouter, Route } from "react-router-dom";
+import Header from "./Header";
+import {
+  StreamCreate,
+  StreamDelete,
+  StreamEdit,
+  StreamList,
+  StreamShow
+} from "./streams/";
+
+const App = () => {
+  return (
+    <div class="ui container">
+      <BrowserRouter>
+        <Header />
+        <Route path="/" exact component={StreamList} />
+        <Route path="/streams/new" exact component={StreamCreate} />
+        <Route path="/streams/edit" exact component={StreamEdit} />
+        <Route path="/streams/delete" exact component={StreamDelete} />
+        <Route path="/streams/show" exact component={StreamShow} />
+      </BrowserRouter>
+    </div>
+  );
+};
+
+export default App;
---
+++ src/components/streams/index.js
@@ -0,0 +1,7 @@
+import StreamCreate from "./StreamCreate";
+import StreamEdit from "./StreamEdit";
+import StreamDelete from "./StreamDelete";
+import StreamList from "./StreamList";
+import StreamShow from "./StreamShow";
+
+export { StreamCreate, StreamDelete, StreamEdit, StreamList, StreamShow };
